c22bd87d631661bb241ee81607f920d6da879f12
Matheus Bertemes <mathbertemes@gmail.com>
2022-03-03 11:10:30 -0300

    Aula 2 - (Telas, autenticação, navegação, contexto e hooks)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { auth, firebase } from './services/firebase';
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+
+
 function App() {
+
+
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </AuthContextProvider>
+    </BrowserRouter>
+    
   );
 }
 
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -21,6 +21,9 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig)
+
 const auth = firebase.auth()
 const database = firebase.database()
+
+export { firebase, auth, database }
 
